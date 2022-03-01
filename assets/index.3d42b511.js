1b191bf82171b94ea5d010fecff567295b08055b
cleisonsantos <css.cleison@outlook.com>
2022-03-01 12:41:13 -0300

    deploy

--- assets/index.3d42b511.js
+++ assets/index.3d42b511.js
@@ -1,4 +1,4 @@
-import{j as d,r as a,s as u,W as h,R as p,a as x}from"./vendor.113ef8a1.js";const g=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))n(r);new MutationObserver(r=>{for(const i of r)if(i.type==="childList")for(const c of i.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function s(r){const i={};return r.integrity&&(i.integrity=r.integrity),r.referrerpolicy&&(i.referrerPolicy=r.referrerpolicy),r.crossorigin==="use-credentials"?i.credentials="include":r.crossorigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(r){if(r.ep)return;r.ep=!0;const i=s(r);fetch(r.href,i)}};g();const e=d.exports.jsx,l=d.exports.jsxs,f=d.exports.Fragment,m=a.exports.createContext({}),y=()=>a.exports.useContext(m),b=({children:o})=>{const[t,s]=a.exports.useState(""),n=r=>{s(r)};return e(m.Provider,{value:{permission:t,handlePermission:n},children:o})},v=u.input`
+import{j as d,r as a,s as u,W as h,R as p,a as g}from"./vendor.113ef8a1.js";const x=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))n(r);new MutationObserver(r=>{for(const i of r)if(i.type==="childList")for(const c of i.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function s(r){const i={};return r.integrity&&(i.integrity=r.integrity),r.referrerpolicy&&(i.referrerPolicy=r.referrerpolicy),r.crossorigin==="use-credentials"?i.credentials="include":r.crossorigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(r){if(r.ep)return;r.ep=!0;const i=s(r);fetch(r.href,i)}};x();const e=d.exports.jsx,l=d.exports.jsxs,f=d.exports.Fragment,m=a.exports.createContext({}),y=()=>a.exports.useContext(m),b=({children:o})=>{const[t,s]=a.exports.useState(""),n=r=>{s(r)};return e(m.Provider,{value:{permission:t,handlePermission:n},children:o})},v=u.input`
         background-color: #333;
         border: none;
         border-radius: 16px;
@@ -7,16 +7,17 @@ import{j as d,r as a,s as u,W as h,R as p,a as x}from"./vendor.113ef8a1.js";cons
         font-size: 48px;
         font-family: 'Cascadia Code';
         outline: 0px;
-        width: 30rem;
+        width: 100%;
+        padding: 0;
         letter-spacing: 10px;
+        margin-bottom: 1rem;
         ::-webkit-inner-spin-button {
             -webkit-appearance: none;
             margin: 0;
         }
         -moz-appearance: textfield;
     `,P=()=>{const{permission:o,handlePermission:t}=a.exports.useContext(m);return e("div",{children:e(v,{type:"number",name:"octal",id:"octal",min:0,maxLength:3,onChange:async n=>{n.target.value.length<=3&&t(n.target.value),n.target.value=n.target.value.substr(0,3)}})})},A=u.table`
-    margin: 10px;
-    width: 25rem;
+    width: auto;
     thead {
         border-bottom: 1px #e8e6e3 solid;
     }
@@ -41,7 +42,7 @@ import{j as d,r as a,s as u,W as h,R as p,a as x}from"./vendor.113ef8a1.js";cons
             sans-serif;
     }
     h1 {
-        font-size: 3.5rem;
+        font-size: 3rem;
         margin-bottom: 1rem;
     }
 `,E=u.main`
@@ -50,4 +51,5 @@ import{j as d,r as a,s as u,W as h,R as p,a as x}from"./vendor.113ef8a1.js";cons
     align-items: center;
     flex-direction: column;
     min-height: 100vh;
-`;function I(){return l(f,{children:[e(C,{}),e(b,{children:l(E,{children:[e("h1",{children:"Chmod Guru!!!"}),e("p",{children:"Um simples utilit\xE1rio para saber qual permiss\xE3o \xE9 do Linux!!!"}),e(P,{}),e(S,{})]})})]})}p.render(e(x.StrictMode,{children:e(I,{})}),document.getElementById("root"));
+    padding: .25rem;
+`;function I(){return l(f,{children:[e(C,{}),e(b,{children:l(E,{children:[e("h1",{children:"Chmod Guru!!!"}),e("p",{children:"Um simples utilit\xE1rio para saber qual \xE9 permiss\xE3o do Linux!!!"}),e(P,{}),e(S,{})]})})]})}p.render(e(g.StrictMode,{children:e(I,{})}),document.getElementById("root"));
